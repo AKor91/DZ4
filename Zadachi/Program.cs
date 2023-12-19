c7a39e50e8139989c1c6c7255be91a87f837b267
Aleksandr Korshunov <akor1191@mail.ru>
2023-12-19 12:06:55 +0300

    Added Zadacha 1

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Console.Write("Укажите номер строки ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Укажите номер столбца ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int[3,4];
+
+// void CreateArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = new Random().Next(1,11);
+//         }
+//     }
+// }
+
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write(array[i,j] + " ");
+//         }
+// System.Console.WriteLine();
+//     }
+// System.Console.WriteLine();
+// }
+
+// CreateArray();
+// PrintArray();
+
+// if(num1 >= 0 && num1 < array.GetLength(0) && num2 >=0 && num2 < array.GetLength(1))
+//     {
+//         int num3 = array[num1,num2];
+//         Console.WriteLine("Элемент массива = " +num3);
+//     }
+// else
+//     {
+//         Console.WriteLine("Такого элемента нет");
+//     }
